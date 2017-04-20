910d9786751e395b889f99273ce3966c61a0c607
Tunous <lukus178@gmail.com>
2017-04-20 11:19:39 +0200

    Move upload action to FAB

--- app/src/main/kotlin/me/thanel/quickimage/MainActivity.kt
+++ app/src/main/kotlin/me/thanel/quickimage/MainActivity.kt
@@ -5,23 +5,18 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.view.View
 import android.widget.Toast
-import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
 import me.thanel.quickimage.extensions.copyTextToClipboard
 import me.thanel.quickimage.uploader.ImageUploader
 import me.thanel.quickimage.uploader.imgur.ImgurImageUploader
 
 class MainActivity : AppCompatActivity(), View.OnClickListener, ImageUploader.Callback {
-    private var imageUri: Uri? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        image.setOnClickListener(this)
         uploadButton.setOnClickListener(this)
     }
 
@@ -31,19 +26,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, ImageUploader.Ca
         if (requestCode != REQUEST_PICK_FILE) return
         if (resultCode != Activity.RESULT_OK) return
 
-        imageUri = data?.data ?: return
-
-        Picasso.with(this)
-                .load(imageUri)
-                .placeholder(R.drawable.ic_image)
-                .fit()
-                .into(image)
+        val imageUri = data?.data ?: return
+        uploadImage(imageUri)
     }
 
     override fun onClick(v: View) {
         when (v.id) {
-            R.id.image -> chooseImage()
-            R.id.uploadButton -> uploadImage()
+            R.id.uploadButton -> chooseImage()
         }
     }
 
@@ -54,24 +43,18 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, ImageUploader.Ca
         startActivityForResult(intent, REQUEST_PICK_FILE)
     }
 
-    private fun uploadImage() {
-        val uri = imageUri ?: return
+    private fun uploadImage(imageUri: Uri) {
+        if (!ExternalStoragePermissionActivity.askForPermission(this, imageUri)) return
 
-        if (!ExternalStoragePermissionActivity.askForPermission(this, uri)) return
-
-        ImgurImageUploader(this, this).uploadImage(uri)
+        ImgurImageUploader(this, this).uploadImage(imageUri)
     }
 
     override fun onSuccess(link: String) {
-        Toast.makeText(this, "Upload finished: $link", Toast.LENGTH_SHORT).show()
-        Log.d("ImageUpload", "Upload finished: $link")
-
         copyTextToClipboard("Image link", link)
     }
 
     override fun onFailure() {
         Toast.makeText(this, "Error uploading image", Toast.LENGTH_SHORT).show()
-        Log.d("ImageUpload", "Error uploading image")
     }
 
     companion object {
